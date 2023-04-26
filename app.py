afffd311efe6a68417b670b153631c71aefb7688
gogo-yubari <124599667+gogo-yubari@users.noreply.github.com>
2023-04-26 12:51:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import time
-
-'Starting a long computation...'
-
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.05)
-
-'...and now we're done!'
 
 st.title('Draw a line chart')
 
